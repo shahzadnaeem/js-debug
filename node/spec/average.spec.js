171b19f9688c6d68e2763e6e7dd4ca8c36965770
Shahzad Naeem <shahzad65naeem@gmail.com>
2022-06-22 09:42:32 +0100

    Use jasmine-data-provider and tweak debug.js example

--- node/spec/average.spec.js
+++ node/spec/average.spec.js
@@ -1,4 +1,29 @@
+const using = require('jasmine-data-provider');
+
 const average = require("../src/lib/average");
+
+// Create an array with values from 'start' to 'end' inclusive
+function range(start, end, step) {
+  if ( step === 0 ) {
+    throw new RangeError('Range cannot have 0 step!');
+  }
+  return Array.from( { length: (end-start)/step + 1}, (_,i) => start + i * step )
+}
+
+function numRangeProvider() {
+  const rows = []
+  const upTos = [0,1,2,3,5,10,100,200,500,1000]
+
+  upTos.forEach(n => {
+    const expectedSum = (n * (n+1))/2;
+    const expectedAverage = n === 0 ? 0 : expectedSum / n;
+    const input = n === 0 ? [] : range( 1, n, 1 );
+
+    rows.push({ input, expectedAverage })
+  });
+  
+  return rows;
+}
 
 describe("Average tests", () => {
   it("Average an empty arrray", () => {
@@ -9,4 +34,34 @@ describe("Average tests", () => {
 
     expect(result).toEqual(expectedAverage);
   });
+
+  it("Average repeated number is number itself", () => {
+    const num = 345;
+    const n = 1000;
+    const expectedAverage = num;
+    const input = Array(n).fill(num);   // Create an array of size 'n' with all values of 'num'
+
+    const result = average(input);
+
+    expect(result).toEqual(expectedAverage);
+  });
+
+  it("Average of array with sum of 0 is 0", () => {
+    const expectedAverage = 0;
+    const input = [-3,-2,-100,0,100,2,3];
+
+    const result = average(input);
+
+    expect(result).toEqual(expectedAverage);
+  });
+
+  it("Average 1..n array", () => {
+    using( numRangeProvider, (data) => {
+
+      const result = average( data.input )
+
+      expect(result).toEqual(data.expectedAverage);
+    })
+  });
+
 });
--- node/src/debug.js
+++ node/src/debug.js
@@ -1,3 +1,6 @@
+
+// Some functions we have written
+
 function add(x, y) {
   const sum = x + y;
 
@@ -12,19 +15,57 @@ function multiply(x, y) {
 
 function badAdd(x, y) {
   const sum = x + x;
+
+  return sum
 }
+
+function badMultiply(x, y) {
+  const product = x * y;
+}
+
+// A test helper function - outputs PASS/FAIL and details of function and result
+
+function check( name, x, y, result, expectedResult ) {
+
+  const success = result === expectedResult
+
+  let output = ''
+
+  if ( success ) {
+    output += "PASS"
+  } else {
+    output += "FAIL"
+  }
+
+  output += `: ${name}(${x},${y})`
+  if ( success ) {
+    output += ` === ${expectedResult}`
+  } else {
+    output += ` !== ${expectedResult} (got ${result})`
+  }
+
+   console.log(output);
+
+}
+
+// Some test input values
 
 const x = 1;
 const y = 2;
 
-const sum = add(x, y);
+// The expected results
 
-console.log(`x=${x}, y=${y}, add(${x},${y}) = ${sum}`);
+expectedSum = 3;
+expectedProduct = 2;
+
+const sum = add(x, y);
+check( "add", x, y, sum, expectedSum )
 
 const badSum = badAdd(x, y);
-
-console.log(`x=${x}, y=${y}, badAdd(${x},${y}) = ${badSum}`);
+check( "badAdd", x, y, badSum, expectedSum )
 
 const product = multiply(x, y);
+check( "multiply", x, y, product, expectedProduct )
 
-console.log(`x=${x}, y=${y}, multiply(${x},${y}) = ${product}`);
+const badProduct = badMultiply(x, y);
+check( "badMultiply", x, y, badProduct, expectedProduct )
